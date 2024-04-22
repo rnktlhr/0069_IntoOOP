3712f55d896886f424e53e94c0291265429e07d0
rnktlhr <rinakitluhur10@gmail.com>
2024-04-22 15:05:41 +0700

    menambahkan void input

--- contoh2.cpp
+++ contoh2.cpp
@@ -6,4 +6,11 @@ class bangunDatar{
         float panjang, lebar;
     public: 
         float Luas;
+    
+    void input(){
+        cout << "Masukkan Panjangnya = ";
+        cin >> panjang;
+        cout << "Masukkan Lebarnya = ";
+        cin >> lebar;
+    }
 };
